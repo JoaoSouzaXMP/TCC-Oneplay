557b4e2b3aa55c2056a15db49595a9e32600709e
João Vitor de Souza <163756483+JoaoSouzaXMP@users.noreply.github.com>
2025-03-17 22:01:12 -0300

    reajuste de arquivos errados

--- modelos/__init__.py
+++
@@ -1,9 +0,0 @@
-# modelos/__init__.py
-from .Jogo import Jogo
-from .PaginaJogo import PaginaJogo
-from .ImagemJogo import ImagemJogo
-from .TutorialJogo import TutorialJogo
-from .Usuario import Usuario
-
-from .Index import Index
-from .Pagina import Pagina
